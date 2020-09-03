e03923c27c614ba0ff90dfea2e2813d235f1eceb
walambert <walambert.work@gmail.com>
2020-09-03 00:50:45 -0500

    keyboard_input function created; exits with no output or input requests when run so isn't working at the moment

--- code/mean.py
+++ code/mean.py
@@ -2,10 +2,12 @@
 
 #Source - Keyboard input
 #variables: list that will take arbitrary number of inputted float values
-def keyboard_input(nums = [], total):
-    total = int(input("How many numbers are you inputting?"))
-
-    while n <= total:
+def keyboard_input():
+    count = int(input("How many numbers are you inputting?"))
+    nums = []
+    while n <= count:
         nums.append(float(input("Please enter number: ")))
         n += 1
-        
+
+    mean = sum(nums) / count
+    return mean
