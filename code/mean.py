290be11f3f60d9beedc289ded14eeb8484ff301b
walambert <walambert.work@gmail.com>
2020-09-03 00:43:13 -0500

    began writing keyboard input function

--- code/mean.py
+++ code/mean.py
@@ -1,1 +1,11 @@
-#Simple mean calculator with variety of input methods.
+#Mean calculator with variety of input methods.
+
+#Source - Keyboard input
+#variables: list that will take arbitrary number of inputted float values
+def keyboard_input(nums = [], total):
+    total = int(input("How many numbers are you inputting?"))
+
+    while n <= total:
+        nums.append(float(input("Please enter number: ")))
+        n += 1
+        
